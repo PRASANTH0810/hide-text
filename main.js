33ed68a4b9c19ea141f0eb99437c88dacd69fa6f
Prasanth Reddy Kolakalapudi <135305652+PRASANTH0810@users.noreply.github.com>
2024-01-22 15:28:48 +0530

    Add files via upload
    hide/display text.

---
+++ main.js
@@ -0,0 +1,8 @@
+function hide() {
+  var element = document.getElementById("hideme");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
